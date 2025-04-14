df9a815127f952539bc471c81266de154ac5f874
ellkbae <59190229+ellkbae@users.noreply.github.com>
2025-04-13 21:03:59 -0400

    Update core.py

--- TeamLFG/core.py
+++ TeamLFG/core.py
@@ -34,7 +34,7 @@ DEFAULT_IMAGE_URL = "https://media.discordapp.net/attachments/135307774774817600
 class LFGView(View):
     def __init__(self, invite_url: str):
         super().__init__(timeout=None)
-        self.add_item(Button(label="Join Party", url=invite_url, style=discord.ButtonStyle.link, emoji={winning_emoji}))
+        self.add_item(Button(label="Join Party", url=invite_url, style=discord.ButtonStyle.link, emoji=f"{winning_emoji}"))
 
 
 def load_config():
